c25c6fe862ce9df6a72b442d89978c76f6d7e8c2
Bulhwi Cha <chabulhwi@semmalgil.com>
2023-10-27 15:52:28 +0900

    feat: add St. Anselm's ontological argument
    This might be the first attempt in the Lean community to formalize an
    ontological argument for the existence of God in Lean. It's a modified
    version of the formulation of St. Anselm's ontological argument by Alvin
    Plantinga. (See 9.1 Formulation 1 of the SEP entry "Ontological
    Arguments.")
    
    1. God exists in the understanding but not in reality. (Assumption for
       *reductio*)
    2. Existence in reality is greater than existence in the understanding
       alone. (Premise)
    3. There exists a being in reality that can be conceived. (*Modified*
       premise)
    4. A being that exists in reality and can be conceived is greater than
       God. (From (1) and (2).)
    5. There exists a being that can be conceived and is greater than God.
       (From (3) and (4).)
    6. No being greater than God can be conceived. (From the definition of
       “God”.)
    7. Hence, it is false that God exists in the understanding but not in
       reality. (From (1), (5), (6).)
    8. God exists in the understanding. (Premise, to which even the Fool
       agrees.)
    9. Hence God exists in reality. (From (7), (8).)
    
    Step 3 of the original formulation by Alvin Plantinga is the following
    premise: "A being having all of God’s properties plus existence in
    reality can be conceived." This premise is redundant. Since God is the
    greatest being that can be conceived, God itself can be conceived.
    Therefore, a being having all of God's properties can be conceived.
    
    I think we can only conclude from St. Anselm's argument that the
    following statement is true:
    ```lean
    theorem isGod_inReality {x : Being} : isGod x → inReality x
    ```
    This theorem doesn't state that God exists in reality. It merely says
    that if a being is God, it exists in reality. In order to prove the
    existence of God in reality, you need to show that `∃ (x : Being),
    isGod x ∧ inReality x`.
    
    For this reason, I think St. Anselm's argument doesn't show God's
    existence. Of course, I might have formalized it wrong.
    
    **Reference**
    
    Oppy, Graham, "Ontological Arguments", *The Stanford Encyclopedia of
    Philosophy* (Fall 2023 Edition), Edward N. Zalta & Uri Nodelman (eds.),
    URL = https://plato.stanford.edu/archives/fall2023/entries/ontological-arguments
    
    **Acknowledgements**
    
    I'd like to thank Eric Wieser and Alistair Tucker. Eric Wieser found
    inconsistency in my first and second drafts. Alistair Tucker gave a
    comment on transcribing premise 3 of the original formulation by Alvin
    Plantinga. You can see my discussion with them in this link: https://leanprover.zulipchat.com/#narrow/stream/113488-general/topic/Formalizing.20St.2E.20Anselm's.20ontological.20argument/near/39867934

---
+++ Notes/Anselm.lean
@@ -0,0 +1,64 @@
+import Mathlib.Order.Basic
+
+section
+
+variable {α : Type u} [LinearOrder α]
+
+def isMax (x : α) (p : α → Prop) := p x ∧ ∀ (y : α), p y → y ≤ x
+
+theorem isMax_unique {x y : α} (p : α → Prop) : isMax x p ∧ isMax y p → x = y := by
+  intro ⟨hx, hy⟩
+  have x_le_y : x ≤ y := hy.2 x hx.1
+  have y_le_x : y ≤ x := hx.2 y hy.1
+  exact le_antisymm x_le_y y_le_x
+
+theorem isMax_mt {x y : α} (p : α → Prop) (hmx : isMax x p) (hlt : x < y) : ¬ p y :=
+  mt (hmx.2 y) (not_le_of_lt hlt)
+
+end
+
+theorem isMax_property (α : Type u) [LinearOrder α] {x : α} (p q : α → Prop) (hmx : isMax x p)
+    (hlt : ∀ {y : α}, ¬ q x → q y → x < y) (hex : ∃ (y : α), p y ∧ q y) : q x := by
+  have hnnqx : ¬¬ q x := by
+    intro hnqx
+    rcases hex with ⟨y, hpy, hqy⟩
+    have x_lt_y : x < y := hlt hnqx hqy
+    have y_le_x : y ≤ x := hmx.2 y hpy
+    exact not_le_of_lt x_lt_y y_le_x
+  exact of_not_not hnnqx
+
+class Anselm (Being : Type u) extends LinearOrder Being where
+  conceivable : Being → Prop
+  inUnderstanding : Being → Prop
+  inReality : Being → Prop
+  lt_of_inUnderstanding_not_inReality_inReality {x y : Being} : inUnderstanding x → ¬ inReality x →
+    inReality y → x < y
+  isMax_conceivable_inUnderstanding {x : Being} : isMax x conceivable → inUnderstanding x
+  exists_conceivable_and_inReality : ∃ (x : Being), conceivable x ∧ inReality x
+
+namespace Anselm
+
+variable {Being : Type u} [Anselm Being]
+
+def isGod (x : Being) := isMax x conceivable
+
+theorem isGod_unique {x y : Being} : isGod x ∧ isGod y → x = y := isMax_unique conceivable
+
+theorem isGod_mt {x y : Being} (hgx : isGod x) (hlt : x < y) : ¬ conceivable y :=
+  isMax_mt conceivable hgx hlt
+
+theorem isGod_inUnderstanding {x : Being} : isGod x → inUnderstanding x :=
+  isMax_conceivable_inUnderstanding
+
+theorem isGod_conceivable {x : Being} : isGod x → conceivable x := And.left
+
+theorem isGod_inReality {x : Being} (hgx : isGod x) : inReality x := by
+  refine isMax_property Being conceivable inReality hgx ?_ ?_
+  · intro y
+    exact lt_of_inUnderstanding_not_inReality_inReality (isGod_inUnderstanding hgx)
+  · exact exists_conceivable_and_inReality
+
+#print axioms isGod_inReality
+-- 'Anselm.isGod_inReality' depends on axioms: [Classical.choice, Quot.sound, propext]
+
+end Anselm
