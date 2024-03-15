8c842f6b682a190996508c8e33314764a2cf83d4
Bulhwi Cha <chabulhwi@semmalgil.com>
2024-03-15 12:24:18 +0900

    chore: update `lakefile.lean`

--- lakefile.lean
+++ lakefile.lean
@@ -1,8 +1,13 @@
 import Lake
 open Lake DSL
 
-package leannotes where
-  -- add any package configuration options here
+package «lean-notes» where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩
+  ]
+  -- add any additional package configuration options here
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
@@ -12,5 +17,5 @@ require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
 
 @[default_target]
-lean_lib Notes where
+lean_lib «Notes» where
   -- add any library configuration options here
