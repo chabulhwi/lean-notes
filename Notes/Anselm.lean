05ba62f3262e60f007b14a5bbeeceb4e571eee59
Bulhwi Cha <chabulhwi@semmalgil.com>
2024-04-07 17:41:09 +0900

    chore: move toolchain to v4.7.0

--- Notes/Anselm.lean
+++ Notes/Anselm.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Bulhwi Cha. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bulhwi Cha
 -/
-import Mathlib.Init.Data.Int.CompLemmas
+import Mathlib.Data.Int.Defs
 import Mathlib.Order.Bounds.Basic
 
 /-!
